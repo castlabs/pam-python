9900d87fd9c902996e1b00fdd78a61bff1ea1b06
Anders Blomdell <anders.blomdell@gmail.com>
2020-08-04 13:35:51 +0200

    Switch to using LDVERSION for -lpythonX.Y (needed for fedora31: -lpython3.7m)

--- src/setup.py
+++ src/setup.py
@@ -31,7 +31,7 @@ else:
   Py_DEBUG = [('Py_DEBUG',1)]
 
 libpython_so = distutils.sysconfig.get_config_var('INSTSONAME')
-python_version = distutils.sysconfig.get_python_version()
+python_ldversion = distutils.sysconfig.get_config_var('LDVERSION')
 ext_modules = [
     Extension(
       "pam_python3",
@@ -39,7 +39,7 @@ ext_modules = [
       include_dirs = [],
       library_dirs=[],
       define_macros=[('LIBPYTHON_SO','"'+libpython_so+'"')] + Py_DEBUG,
-      libraries=["pam", "python"+python_version],
+      libraries=["pam", "python"+python_ldversion],
     ), ]
 
 setup(
